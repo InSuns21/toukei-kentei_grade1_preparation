7c52784b83c6dbb08b84acfc459ca71059a3e6d6
InSuns21 <senkonoshine@yahoo.co.jp>
2026-09-05 22:51:07 +0900

    Remove temporary S6 terminology script

--- scripts/tmp-s6-terminology-fix.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-
-const path = 'textbook/volumes/04_linear_models/L1_04_回帰診断_一般化最小二乗_正則化/index.md';
-let text = fs.readFileSync(path, 'utf8');
-const before = '$\\Omega=I_n$ なら $Q(\\beta)=\\|y-X\\beta\\|^2$ となるので、GLS は通常最小二乗法に戻ります。';
-const after = '$\\Omega=I_n$ なら $Q(\\beta)=\\|y-X\\beta\\|^2$ となるので、一般化最小二乗法は通常最小二乗法に戻ります。';
-if (text.includes(before)) text = text.replace(before, () => after);
-else if (!text.includes(after)) throw new Error('GLS terminology target not found');
-fs.writeFileSync(path, text, 'utf8');
